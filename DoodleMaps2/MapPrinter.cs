a528b1a236f177c18e8a5e8437915bbf7de79266
Anastasiiasyvak <asyvak@kse.org.ua>
2023-03-03 18:53:05 +0200

    3, 5 points

--- DoodleMaps2/MapPrinter.cs
+++ DoodleMaps2/MapPrinter.cs
@@ -5,15 +5,38 @@
 
     public class MapPrinter
     {
-        public void Print(string[,] maze)
+        // maze представляє карту лабіринта 
+        // track список координат точок, через які проходять шлях
+        public void Print(string[,] maze, List<Point> track)  
         {
-            PrintTopLine();
+            var start = track[0]; // координати стартової точки
+            var goal = track[^1];  // повертає останній символ із списку track
+            PrintTopLine(); // метод який відображає відступи для кожного рядка, зокрема- координати верхньої лінії
             for (var row = 0; row < maze.GetLength(1); row++)
             {
-                Console.Write($"{row}\t");
+                Console.Write($"{row}\t"); // формує рядок який містить номер рядка
                 for (var column = 0; column < maze.GetLength(0); column++)
                 {
-                    Console.Write(maze[column, row]);
+                    if (start.Equals(new Point(column, row)))
+                    {
+                        Console.Write('A');
+                    }
+                    else if (goal.Equals(new Point(column, row)))
+                    {
+                        Console.Write('B');
+                    }
+                    // else if (track.Contains(new Point(column, row)))
+                    // {
+                    //     Console.Write('.');
+                    // }
+                    else if (maze[column, row] != "█")
+                    {
+                        Console.Write('.');
+                    }
+                    else
+                    {
+                        Console.Write(maze[column, row]);
+                    }
                 }
 
                 Console.WriteLine();
--- DoodleMaps2/Program.cs
+++ DoodleMaps2/Program.cs
@@ -1,3 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using Kse.Algorithms.Samples;
+// MapGenerator клас, а MapGeneratorOptions це об'єкт через який передається ширина і довжина 
+var generator = new MapGenerator(new MapGeneratorOptions()
+{
+     Height = 10,
+     Width = 15,
+     Seed = 3, // якщо не задавати seed то карта буде кожен раз різна 
+     Noise = 0.1f // стінки наші, самий плотний це 0 і до 1 можна обирати
+});
+// Generate використовується для створення двовимірного масиву 
+// Print друкує мапу 
+// кома вказує на те що массив є двовимірним string[,] всі елементи масиву мають тип строки
+string[,] map = generator.Generate();
+List<Point> track = new List<Point>();
 
-Console.WriteLine("Hello, World!");
+Point start = new Point(column: 0, row: 2);
+Point goal = new Point(column: 14, row: 8);
+track.Add(start);
+track.Add(new Point(6, 6));
+track.Add(goal);
+
+
+new MapPrinter().Print(map, track);
+
+// List<Point> GetShortestPath(string[,] map, Point start, Point goal)
+// {
+//      List<Point> result = new List<Point>();
+//      return result;
+//      // your code here
+// }
+
