35c3cb8b3b0db6d867ba4248cbbade5cadbbefe3
Anastasiiasyvak <asyvak@kse.org.ua>
2023-03-16 21:26:00 +0200

    new commit

--- DoodleMaps2/MapPrinter.cs
+++ DoodleMaps2/MapPrinter.cs
@@ -5,36 +5,36 @@
 
     public class MapPrinter
     {
-        // maze представляє карту лабіринта 
-        // track список координат точок, через які проходять шлях
-        public void Print(string[,] maze, List<Point> path)  
+        // maze двовимірний масив рядків, представляє карту лабіринта 
+        // path список координат точок, через які проходять шлях
+        // метод Print
+        public void Print(string[,] maze, List<Point> path)
         {
             PrintTopLine();
-            var start = path[0]; // координати стартової точки
-            var goal = path[^1];  // повертає останній символ із списку track
-            PrintTopLine(); // метод який відображає відступи для кожного рядка, зокрема- координати верхньої лінії
+            var start = path[0];
+            var goal = path[^1];
             for (var row = 0; row < maze.GetLength(1); row++)
             {
                 Console.Write($"{row}\t"); // формує рядок який містить номер рядка
                 for (var column = 0; column < maze.GetLength(0); column++)
                 {
-                    var cur_point = new Point(row: row, column: column);
-                    if (start.Equals(cur_point))
+                    var current_point = new Point(column: column, row: row);
+                    if (start.Equals(current_point))
                     {
                         Console.Write('B');
                     }
-                        else if (goal.Equals(cur_point))
-                        {
+                    else if (goal.Equals(current_point))
+                    {
                         Console.Write('A');
-                        }
-                            else if (path.Contains(cur_point))
-                            {
-                                Console.Write(".");
-                            }
-                            else
-                            {
-                                Console.Write(maze[column, row]);
-                            }
+                    }
+                    else if (path.Contains(current_point))
+                    {
+                        Console.Write(".");
+                    }
+                    else
+                    {
+                        Console.Write(maze[column, row]);
+                    }
 
                 }
 
@@ -43,20 +43,22 @@
 
             void PrintTopLine()
             {
-                Console.Write($" \t");
+                Console.Write(" \t");
                 for (int i = 0; i < maze.GetLength(0); i++)
                 {
-                    Console.Write(i % 10 == 0? i / 10 : " ");
+                    Console.Write(i % 10 == 0 ? i / 10 : " ");
                 }
-    
-                Console.Write($"\n \t");
+
+                Console.Write("\n \t");
                 for (int i = 0; i < maze.GetLength(0); i++)
                 {
                     Console.Write(i % 10);
                 }
-    
+
                 Console.WriteLine("\n");
             }
         }
     }
 }
+
+
--- DoodleMaps2/Program.cs
+++ DoodleMaps2/Program.cs
@@ -1,35 +1,32 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
-using Kse.Algorithms.Samples;
-// MapGenerator клас, а MapGeneratorOptions це об'єкт через який передається ширина і довжина 
+﻿using Kse.Algorithms.Samples;
 var generator = new MapGenerator(new MapGeneratorOptions()
 {
      Height = 10,
      Width = 15,
-     Seed = 3, // якщо не задавати seed то карта буде кожен раз різна 
-     Noise = 0.1f // стінки наші, самий плотний це 0 і до 1 можна обирати
+     Seed = 3,  
+     Noise = 0.1f,
+     AddTraffic = true,
+     TrafficSeed = 12345
 });
 // Generate використовується для створення двовимірного масиву 
-// Print друкує мапу 
 // кома вказує на те що массив є двовимірним string[,] всі елементи масиву мають тип строки
 string[,] map = generator.Generate();
-List<Point> track = new List<Point>();
-var distances = new Dictionary<Point, int>();
-var origins = new Dictionary<Point, Point>();
+var distances = new Dictionary<Point, int>(); // зберігає відстань від початкової точки до кожної відвіданої точки на карті
+                                              // Ключами словника є точки, які представлені об'єктами класу Point, а значеннями - відстані від початкової точки до цих точок.
+var origins = new Dictionary<Point, Point>(); // зберігає інформацію про те, з якої точки на карті була відвідана кожна точка. 
+//Ключами словника також є об'єкти класу Point, а значеннями - об'єкти Point, які представляють точки, з яких була відвідана поточна точка.
 var start = new Point(column: 0, row: 2);
-var goal = new Point(row: 8, column: 9);
+var goal = new Point(row: 8, column: 2);
 distances[start] = 0;
-// track.Add(start);
-// track.Add(goal);
 
 
 var my_result = GetPath(map, start, goal);
 
-// new MapPrinter().Print(map, track);
+
 
 List<Point> GetPath(string[,] map, Point start, Point goal)
 {
      var path = BFS(start, goal);
-     // List<Point> path = new List<Point>();
      new MapPrinter().Print(map, path);
      return path;
 }
@@ -38,21 +35,22 @@ List<Point> GetPath(string[,] map, Point start, Point goal)
 List<Point> BFS(Point start, Point goal)
 {
      var visited = new List<Point>();
-     var queue = new Queue<Point>();
-     List<Point> path = new List<Point>();
+     var queue = new Queue<Point>(); // використовується для зберігання точок, що треба дослідити
+     var path = new List<Point>(); // містить всі точки, що складають найкоротший шлях від стартової до цільової точки
      
      queue.Enqueue(start);
      Visit(start);
      bool stop = false;
      while (queue.Count > 0)
      {
+          // змінна next це поточна точка яка буде використовуватися для того щоб знайти її сусідів 
           var next = queue.Dequeue();
           var neighbours = GetNeighbours(row:next.Row, column:next.Column, maze:map);
           foreach (var neighbour in neighbours)
           {
                if (!visited.Contains(neighbour))
                {
-                    origins[neighbour] = next; // next це current точка з якою ми працюємо
+                    origins[neighbour] = next; 
                     distances[neighbour] = distances[next] + 1;
                     Visit(neighbour);
                     queue.Enqueue(neighbour);
@@ -82,7 +80,6 @@ List<Point> BFS(Point start, Point goal)
 
      void Visit(Point point)
      {
-          //map[point.Column, point.Row] = distances[point].ToString();
           visited.Add(point);
      }
 }
@@ -99,15 +96,13 @@ List<Point> GetNeighbours(int row, int column, string[,] maze)
 
      void TryAddWithOffset(int offsetRow, int offsetColumn)
      {
+          // row поточне значення рядка
           var newX = row + offsetRow;
           var newY = column + offsetColumn;
-          if (newX >= 0 && newY>= 0 && newX < maze.GetLength(0) && newY < maze.GetLength(1) && maze[newY, newX] != "█")
+          if (newX >= 0 && newY>= 0 && newX < maze.GetLength(1) && newY < maze.GetLength(0) && maze[newY, newX] != "█")
           {
                result.Add(new Point(newY, newX));
           }
      }
-          
-
-
-
+     
 }
